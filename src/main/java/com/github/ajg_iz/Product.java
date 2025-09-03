836277a525a6dd5739c3582358cab63c1d0d9d2e
ajg-iz <andreas.goebel@gmail.com>
2025-09-03 14:37:44 +0200

    Initial commit: Main, Product, ProductRepo

---
+++ src/main/java/com/github/ajg_iz/Product.java
@@ -0,0 +1,4 @@
+package com.github.ajg_iz;
+
+public record Product(int ID, String name, Double price) {
+}
---
+++ src/main/java/com/github/ajg_iz/ProductRepo.java
@@ -0,0 +1,48 @@
+package com.github.ajg_iz;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductRepo {
+    List<Product> products;
+    public ProductRepo() {
+        this.products = new ArrayList<>();
+    }
+    public ProductRepo(List<Product> products) {
+        this.products = products;
+    }
+
+    public boolean addProduct(Product product) {
+        return this.products.add(product);
+    }
+
+    public boolean removeProduct(Product product) {
+        return this.products.remove(product);
+    }
+
+    public Product getProductByName(String  name) {
+        for (Product product : products) {
+            if (product.name().equals(name)) {
+                return product;
+            }
+        }
+        return null;
+    }
+
+    public Product getProductByID(int ID) {
+        for (Product product : products) {
+            if (product.ID() == ID) {
+                return product;
+            }
+        }
+        return null;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+}
